66e0b379083edc28ac67c66c616c68d9ab3393a6
yadavanuj1996 <yadavanuj@hotmail.com>
2019-11-05 10:05:44 +0530

    Add handleClick to update state with current expression.

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import './style.scss';
 class Calculator extends React.Component{
   constructor(props){
     super(props);
+    this.state={expression:'0'};
+    this.handleClick=this.handleClick.bind(this);
+  }
+  handleClick(event){
+    let clickedButton=document.getElementById(event.target.id).innerHTML;
+    this.state.expression=`${this.state.expression}${clickedButton}`;
   }
   render(){
     return(
-      <CalcNumbers/>
+      <CalcNumbers handleClick={this.handleClick}/>
     );
   }
 }
@@ -22,9 +28,9 @@ class CalcNumbers extends React.Component{
    
     return (
       <div className="row w-100 justify-content-center">
-      <CalcTopElements/>
-      <CalcMiddleElements/>
-      <CalcBottomElements/>
+      <CalcTopElements handleClick={this.props.handleClick}/>
+      <CalcMiddleElements handleClick={this.props.handleClick}/>
+      <CalcBottomElements handleClick={this.props.handleClick}/>
       </div>
     );
   }
@@ -37,13 +43,13 @@ class CalcTopElements extends React.Component{
   render(){
     return (
       <div className="row w-100 ">  
-         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="clear">
+         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="clear" onClick={this.props.handleClick}>
           AC
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="backspace">
+        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="backspace" onClick={this.props.handleClick}>
           &#9003;
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="divide">
+        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="divide" onClick={this.props.handleClick}>
           /
         </div>
       </div>
@@ -54,11 +60,12 @@ class CalcMiddleElements extends React.Component{
   constructor(props){
     super(props);
   }
+
   render(){
      let digits=[{7:"seven"},{8:"eight"},{9:"nine"},{x: "multiply"},{4:"four"},{5:"five"},{6:"six"},{"-": "subtract"},{1: "one"},{2:"two"},{3:"three"},{"+": "add"}];
     let digitDiv=digits.map((element)=>{
       return(
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id={`${element[Object.keys(element)[0]]}`}>
+        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id={`${element[Object.keys(element)[0]]}`} onClick={this.props.handleClick}>
           {Object.keys(element)[0]}
         </div>
       );
@@ -77,13 +84,13 @@ class CalcBottomElements extends React.Component{
   render(){
     return (
       <div className="row w-100 ">  
-         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="zero">
+         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="zero" onClick={this.props.handleClick}>
           0
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="decimal">
+        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="decimal" onClick={this.props.handleClick}>
           .
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="equals">
+        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="equals" onClick={this.props.handleClick}> 
           =
         </div>
       </div>

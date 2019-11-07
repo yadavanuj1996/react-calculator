314029f94872be351b16b6fad171d3a6a23914c5
yadavanuj1996 <yadavanuj@hotmail.com>
2019-11-07 08:34:13 +0530

    Remove permission of more than three -ve in expression and correct decimal usage with 0.

--- index.js
+++ index.js
@@ -1,209 +1,298 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.scss';
-import {createStore} from 'redux';
-import {connect} from 'react-redux';
-import {Provider} from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.scss";
+import { createStore } from "redux";
+import { connect } from "react-redux";
+import { Provider } from "react-redux";
 
 
-class Calculator extends React.Component{
-  constructor(props){
+class Calculator extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={expression:'0'};
-    this.handleClick=this.handleClick.bind(this);
-    this.showResults=this.showResults.bind(this);
+    this.state = { expression: "0" };
+    this.handleClick = this.handleClick.bind(this);
+    this.showResults = this.showResults.bind(this);
   }
-  handleClick(event){
-    let clickedButton=document.getElementById(event.target.id).innerHTML;
-    switch(clickedButton.toString()){
-      case 'AC': this.setState({expression: '0'});return;
-      case '⌫':  this.setState({expression: this.state.expression.substring(0,this.state.
-      expression.length-1) });
-                return;
-      case '=': this.showResults();
-                return;
-      case '.': 
-      // if expression contains no arithmetic symbol +,-,/,x
-      if(!(/(?=[\-\+\x\/])/i.test(this.state.expression))){
-        if(/\d+\.\d*$/g.test(this.state.expression))
-          return ;
-      }
-      // if expression contains one or more than one arithmetic symbol +,-,/,x
-      else if(/([\+\-\/\x]\d+\.\d*$)/g.test(this.state.expression)){
+  handleClick(event) {
+    let clickedButton = document.getElementById(event.target.id).innerHTML;
+    switch (clickedButton.toString()) {
+      case "AC":
+        this.setState({ expression: "0" });
         return;
-      }
-      break;
-      case 'x':
-      case '/':
-      case '+':
-        if(this.isInputCharacterSymbol(this.state.expression.slice(-1))){
-          let reducingFactor=1;
-          if(this.isInputCharacterSymbol(this.state.expression.slice(-2,-1))){
-             reducingFactor++;
-             }
-          
-          this.setState({expression: `${this.state.expression.slice(0,this.state.expression.length-reducingFactor)}${clickedButton}`});
+      case "⌫":
+        if(this.state.expression.length===1){
+           this.setState({expression: '0'});
+        }
+        else{
+          this.setState({
+          expression: this.state.expression.substring(
+            0,
+            this.state.expression.length - 1
+          )
+        });
+        
+        }
+        return;
+      case "=":
+        this.showResults();
+        return;
+      case ".":
+        // if decimal does not has preciding number
+        if(/[^\d]/.test(this.state.expression.slice(-1))){
+          return ;
+        }
+
+        // if expression contains no arithmetic symbol +,-,/,x
+        if (!/(?=[\-\+\x\/])/i.test(this.state.expression)) {
+          if (/\d+\.\d*$/g.test(this.state.expression)) return;
+        }
+        // if expression contains one or more than one arithmetic symbol +,-,/,x
+        else if (/([\+\-\/\x]\d+\.\d*$)/g.test(this.state.expression)) {
           return;
         }
         break;
-   
-       
+      case "x":
+      case "/":
+      case "+":
+        if (this.isInputCharacterSymbol(this.state.expression.slice(-1))) {
+          let reducingFactor = 1;
+          if (
+            this.isInputCharacterSymbol(this.state.expression.slice(-2, -1))
+          ) {
+            reducingFactor++;
+          }
+
+          this.setState({
+            expression: `${this.state.expression.slice(
+              0,
+              this.state.expression.length - reducingFactor
+            )}${clickedButton}`
+          });
+          return;
+          
+        }
+        break;
+        case "-":
+        if (this.state.expression.slice(-2)==='--') {
+          this.setState({
+            expression: `${this.state.expression.slice(
+              0,
+              this.state.expression.length - 1
+            )}${clickedButton}`
+          });
+          return;
+          
+        }
+        break;
     }
-    
-    
-    if(this.state.expression==0 && !this.isInputCharacterSymbol(clickedButton))
-      this.setState({expression: `${clickedButton}`});
-    else 
-      this.setState({expression: `${this.state.expression}${clickedButton}`});
+
+    if (
+      this.state.expression == '0' &&
+      !this.isInputCharacterSymbol(clickedButton)
+    )
+      this.setState({ expression: `${clickedButton}` });
+    else
+      this.setState({ expression: `${this.state.expression}${clickedButton}` });
   }
-  isInputCharacterSymbol(inputChar){
+  isInputCharacterSymbol(inputChar) {
     return /[\+\-\x\/.]/.test(inputChar);
   }
-  showResults(){
+  showResults() {
     this.evaluateExpression();
-    this.props.submitExpression(this.state.expression); 
+    this.props.submitExpression(this.state.expression);
   }
-   evaluateExpression(){
+  evaluateExpression() {
     var numRegExp = /(\d+[\.]\d+)|(\d+)/g;
-    var symbolsRegExp=/[\+\-\x\/]{1,2}/g;
+    var symbolsRegExp = /[\+\-\x\/]{1,2}/g;
     var regString = this.state.expression;
-    
 
-    var numbers=(regString.match(numRegExp));
-    var symbols=(regString.match(symbolsRegExp));
+    var numbers = regString.match(numRegExp);
+    var symbols = regString.match(symbolsRegExp);
 
-    for(let i=0;i<symbols.length;i++){
-      let multiplyingFactor=1;
-      if(symbols[i].length===2 && (symbols[i].slice(0,1)==='/' || symbols[i].slice(0,1)==='x')){
-      	symbols[i]=symbols[i].slice(0,1);
-        multiplyingFactor=-1;
+    if (symbols === null || symbols.length == 0 || !(symbols.length+1===numbers.length)) return;
+
+    for (let i = 0; i < symbols.length; i++) {
+      let multiplyingFactor = 1;
+      if (
+        symbols[i].length === 2 &&
+        (symbols[i].slice(0, 1) === "/" || symbols[i].slice(0, 1) === "x")
+      ) {
+        symbols[i] = symbols[i].slice(0, 1);
+        multiplyingFactor = -1;
       }
-      
-      if(symbols[i]==='/'){
-        symbols.splice(i,1);
-        let firstNo=parseFloat(numbers.splice(i,1));
-        let secondNo=parseFloat(numbers[i]);
-        numbers[i]=(firstNo/secondNo)*multiplyingFactor;
+
+      if (symbols[i] === "/") {
+        symbols.splice(i, 1);
+        let firstNo = parseFloat(numbers.splice(i, 1));
+        let secondNo = parseFloat(numbers[i]);
+        numbers[i] = (firstNo / secondNo) * multiplyingFactor;
         i--;
-      }
-      else if(symbols[i]==='x'){
-        symbols.splice(i,1);
-        let firstNo=parseFloat(numbers.splice(i,1));
-        let secondNo=parseFloat(numbers[i]);
-        numbers[i]=(firstNo*secondNo)*multiplyingFactor;
-        i--;
-      }
-      
-    }
-   
-    for(let i=0;i<symbols.length;i++){
-      let multiplyingFactor=1;
-      if(symbols[i].length===2 && (symbols[i].slice(0,1)==='+' || symbols[i].slice(0,1)==='-')){
-      	symbols[i]=symbols[i].slice(0,1);
-        multiplyingFactor=-1;
-      }
-      
-      if(symbols[i]==='+'){
-        symbols.splice(i,1);
-        let firstNo=parseFloat(numbers.splice(i,1));
-        let secondNo=parseFloat(numbers[i]);
-        numbers[i]=firstNo+(secondNo*multiplyingFactor);
-        i--;
-      }
-      else if(symbols[i]==='-'){
-        symbols.splice(i,1);
-        let firstNo=parseFloat(numbers.splice(i,1));
-        let secondNo=parseFloat(numbers[i]);
-        numbers[i]=firstNo-(secondNo*multiplyingFactor);
+      } else if (symbols[i] === "x") {
+        symbols.splice(i, 1);
+        let firstNo = parseFloat(numbers.splice(i, 1));
+        let secondNo = parseFloat(numbers[i]);
+        numbers[i] = firstNo * secondNo * multiplyingFactor;
         i--;
       }
     }
-	
-   
 
-    this.setState({expression: `${numbers[0]}`});
+    for (let i = 0; i < symbols.length; i++) {
+      let multiplyingFactor = 1;
+      if (
+        symbols[i].length === 2 &&
+        (symbols[i].slice(0, 1) === "+" || symbols[i].slice(0, 1) === "-")
+      ) {
+        symbols[i] = symbols[i].slice(0, 1);
+        multiplyingFactor = -1;
+      }
+
+      if (symbols[i] === "+") {
+        symbols.splice(i, 1);
+        let firstNo = parseFloat(numbers.splice(i, 1));
+        let secondNo = parseFloat(numbers[i]);
+        numbers[i] = firstNo + secondNo * multiplyingFactor;
+        i--;
+      } else if (symbols[i] === "-") {
+        symbols.splice(i, 1);
+        let firstNo = parseFloat(numbers.splice(i, 1));
+        let secondNo = parseFloat(numbers[i]);
+        numbers[i] = firstNo - secondNo * multiplyingFactor;
+        i--;
+      }
+    }
+
+    this.setState({ expression: `${parseFloat(numbers[0].toFixed(4))}` }); // parseFloat is used for trailing zeroes
   }
-  
-  render(){
-    return(
-      <CalcNumbers expression={this.state.expression} handleClick={this.handleClick}/>
+
+  render() {
+    return (
+      <CalcNumbers
+        expression={this.state.expression}
+        handleClick={this.handleClick}
+      />
     );
   }
 }
-class CalcNumbers extends React.Component{
-  constructor(props){
+class CalcNumbers extends React.Component {
+  constructor(props) {
     super(props);
   }
-  
-  render(){
-   
+
+  render() {
     return (
       <div className="row w-100 justify-content-center">
-      <CalcTopElements expression={this.props.expression} handleClick={this.props.handleClick}/>
-      <CalcMiddleElements handleClick={this.props.handleClick}/>
-      <CalcBottomElements handleClick={this.props.handleClick}/>
+        <CalcTopElements
+          expression={this.props.expression}
+          handleClick={this.props.handleClick}
+        />
+        <CalcMiddleElements handleClick={this.props.handleClick} />
+        <CalcBottomElements handleClick={this.props.handleClick} />
       </div>
     );
   }
 }
 
-class CalcTopElements extends React.Component{
-  constructor(props){
+class CalcTopElements extends React.Component {
+  constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
-      <div className="row w-100 ">  
-         <div id="display" className="row w-100 justify-content-end align-items-center">{this.props.expression}</div>
-         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="clear" onClick={this.props.handleClick}>
+      <div className="row w-100 ">
+        <div
+          id="display"
+          className="row w-100 justify-content-end align-items-center"
+        >
+          {this.props.expression}
+        </div>
+        <div
+          className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout"
+          id="clear"
+          onClick={this.props.handleClick}
+        >
           AC
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="backspace" onClick={this.props.handleClick}>
+        <div
+          className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout"
+          id="backspace"
+          onClick={this.props.handleClick}
+        >
           &#9003;
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="divide" onClick={this.props.handleClick}>
+        <div
+          className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout"
+          id="divide"
+          onClick={this.props.handleClick}
+        >
           /
         </div>
       </div>
     );
   }
 }
-class CalcMiddleElements extends React.Component{
-  constructor(props){
+class CalcMiddleElements extends React.Component {
+  constructor(props) {
     super(props);
   }
 
-  render(){
-     let digits=[{7:"seven"},{8:"eight"},{9:"nine"},{x: "multiply"},{4:"four"},{5:"five"},{6:"six"},{"-": "subtract"},{1: "one"},{2:"two"},{3:"three"},{"+": "add"}];
-    let digitDiv=digits.map((element)=>{
-      return(
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id={`${element[Object.keys(element)[0]]}`} onClick={this.props.handleClick}>
+  render() {
+    let digits = [
+      { 7: "seven" },
+      { 8: "eight" },
+      { 9: "nine" },
+      { x: "multiply" },
+      { 4: "four" },
+      { 5: "five" },
+      { 6: "six" },
+      { "-": "subtract" },
+      { 1: "one" },
+      { 2: "two" },
+      { 3: "three" },
+      { "+": "add" }
+    ];
+    let digitDiv = digits.map(element => {
+      return (
+        <div
+          className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout"
+          id={`${element[Object.keys(element)[0]]}`}
+          onClick={this.props.handleClick}
+        >
           {Object.keys(element)[0]}
         </div>
       );
     });
     return (
-      <div className="row justify-content-center w-100 h-100"> 
-           {digitDiv}
-      </div>
+      <div className="row justify-content-center w-100 h-100">{digitDiv}</div>
     );
   }
 }
-class CalcBottomElements extends React.Component{
-  constructor(props){
+class CalcBottomElements extends React.Component {
+  constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
-      <div className="row w-100 ">  
-         <div className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout" id="zero" onClick={this.props.handleClick}>
+      <div className="row w-100 ">
+        <div
+          className="text-center theme-bg-color w-50 calc-btn-height  calc-btn-border calc-content-layout"
+          id="zero"
+          onClick={this.props.handleClick}
+        >
           0
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="decimal" onClick={this.props.handleClick}>
+        <div
+          className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout"
+          id="decimal"
+          onClick={this.props.handleClick}
+        >
           .
         </div>
-        <div className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout" id="equals" onClick={this.props.handleClick}> 
+        <div
+          className="text-center theme-bg-color w-25 calc-btn-height  calc-btn-border calc-content-layout"
+          id="equals"
+          onClick={this.props.handleClick}
+        >
           =
         </div>
       </div>
@@ -211,52 +300,51 @@ class CalcBottomElements extends React.Component{
   }
 }
 
-//ReactDOM.render(<Calculator/>,document.getElementById("keypad"));
 
 //Redux Code
 //Action Creator
-const addExpression=(expression)=>{
+const addExpression = expression => {
   return {
-    type: 'expressionCalculation',
+    type: "expressionCalculation",
     expression: expression
-  }
-}
-const DEFAULT_STATE={expression:'0',result: 0};
-const reducer=(state=DEFAULT_STATE,action)=>{
-  if(action.type==="expressionCalculation"){
-    return {expression:'1',result: 1};
-  }
-  else{
+  };
+};
+const DEFAULT_STATE = { expression: "0", result: 0 };
+const reducer = (state = DEFAULT_STATE, action) => {
+  if (action.type === "expressionCalculation") {
+    return { expression: "1", result: 1 };
+  } else {
     return DEFAULT_STATE;
   }
-
 };
+
 
 const store=createStore(reducer);
 
-
-const mapStateToProps = (state) => {
-  return {reduxValue: state}
+const mapStateToProps = state => {
+  return { reduxValue: state };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    submitExpression: (expression) => {
-      dispatch(addExpression(expression))
+    submitExpression: expression => {
+      dispatch(addExpression(expression));
     }
-  }
+  };
 };
 
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Calculator);
+const Container = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Calculator);
 
 class AppWrapper extends React.Component {
   render() {
     return (
       <Provider store={store}>
-	      <Container/>
+        <Container />
       </Provider>
     );
   }
-};
-ReactDOM.render(<AppWrapper/>,document.getElementById("keypad"));
+}
+ReactDOM.render(<AppWrapper />, document.getElementById("keypad"));
